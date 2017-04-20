c2f8c837b16a91eeaaa4df6809d5f57d40444b4d
Christopher <rivas1@cpp.edu>
2017-04-19 21:14:36 -0700

    Successfully reads bytes (100) from Server
    Server sends 100 bytes divided in half though; so the program will read
    from the input stream twice.

--- Ex2Client.java
+++ Ex2Client.java
@@ -1,14 +1,31 @@
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
 
 public final class Ex2Client
 {
-	public static void main (String[] args)
+	public static void main (String[] args) throws IOException
 	{
+		String serverOutput = "";
+		String[] firstHalf = new String[50]; // first 2 bytes of concatenation
+		String[] secondHalf = new String[50]; // second 2 bytes of concatenation
+
 		try
 		{
 			Socket socket = new Socket ("codebank.xyz", 38102);
+			if (socket.isConnected())
+				System.out.println("Connected to server.");	
+
+			System.out.println("Received bytes:");
+
+			{
+				// firstHalf[i] = BR.readLine();
+				// secondHalf[i] = BR.readLine();
+				// System.out.println("First 2 bytes: " + firstHalf[i] + "Second 2 bytes: " + secondHalf[i]); // debug
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
 		}
 		
 	}
